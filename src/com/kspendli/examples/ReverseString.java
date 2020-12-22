dad15717cd3652cc18b42cc86f3714dc6c0008de
kspendli <45584715+kspendli@users.noreply.github.com>
2020-12-22 23:31:50 +0800

    Reverse String Example

---
+++ src/com/kspendli/examples/ReverseString.java
@@ -0,0 +1,24 @@
+package com.kspendli.examples;
+
+public class ReverseString {
+    public static void main(String[] args) {
+        System.out.println("With Using Api Support: "+simpleReverse("kumaraswamypendli"));
+        System.out.println("Without Using Api Support: "+reverseWithoutApi("kumaraswamypendli"));
+    }
+
+    private static String reverseWithoutApi(String input) {
+        char[] inChars = input.toCharArray();
+        char[] reChars = new char[inChars.length];
+        int index = 0;
+        for(int i=inChars.length-1; i >=0;i-- ){
+            reChars[index] = inChars[i];
+            index++;
+        }
+
+        return new String(reChars);
+    }
+
+    private static String simpleReverse(String input){
+        return new StringBuilder(input).reverse().toString();
+    }
+}
